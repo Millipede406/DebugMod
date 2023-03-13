b5560756fe0fee5267cb9535fa8afe2888f9961d
Millipede406 <97822695+Millipede406@users.noreply.github.com>
2023-03-13 19:58:18 +0800

    Mounts can now do infinite damage

--- InfiniteDamage.cs
+++ InfiniteDamage.cs
@@ -6,7 +6,19 @@ namespace DebugMod
     [HarmonyPatch(typeof(Scaling))]
     [HarmonyPatch(nameof(Scaling.BlasterDamageScale))]
     [HarmonyPatch(MethodType.Getter)]
-    public static class InfiniteDamage
+    public static class InfiniteBlasterDamage
+    {
+        public static void Postfix(ref float __result)
+        {
+            if (ModMain.infDamage)
+                __result = int.MaxValue;
+        }
+    }
+
+    [HarmonyPatch(typeof(Scaling))]
+    [HarmonyPatch(nameof(Scaling.RidingDamageScale))]
+    [HarmonyPatch(MethodType.Getter)]
+    public static class InfiniteMountDamage
     {
         public static void Postfix(ref float __result)
         {
