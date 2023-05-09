d7e4443c081487a6c69d328def9a4f798a4763d8
Millipede406 <97822695+Millipede406@users.noreply.github.com>
2023-05-09 17:06:44 +0800

    Create ShinyChancePatch.cs

---
+++ ShinyChancePatch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PatchQuest;
+using HarmonyLib;
+
+namespace DebugMod
+{
+    [HarmonyPatch(typeof(ShinyChance))]
+    [HarmonyPatch(nameof(ShinyChance.BASE_SHINY_CHANCE))]
+    [HarmonyPatch(MethodType.Getter)]
+    public static class ShinyChancePatch
+    {
+        public static void Postfix(ref float __result)
+        {
+            __result = int.MaxValue;
+        }
+    }
+}
