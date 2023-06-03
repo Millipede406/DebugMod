6f5582b2c41a4f31a7bf4e48192420fd8e98cf27
Millipede406 <97822695+Millipede406@users.noreply.github.com>
2023-06-03 17:47:29 +0800

    Laid out DebugMenu class

--- GUI/DebugMenu.cs
+++ GUI/DebugMenu.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace DebugMod.GUI
 {
     public static class DebugMenu
     {
+        private static Rect[] rects =
+        {
+            // Background
+            new Rect(0, 0, 0, 0),
 
+            // Cheats Menu button
+            new Rect(0, 0, 0, 0),
+
+            // Tools Menu button
+            new Rect(0, 0, 0, 0),
+
+            // Toggle Info text
+            new Rect(0, 0, 0, 0)
+        };
+        private static Rect windowRect;
+
+        public static void InitializeMenu()
+        {
+            windowRect = rects[0];
+        }
+        public static void DrawMenu()
+        {
+
+        }
     }
 }
