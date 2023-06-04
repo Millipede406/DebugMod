88f4d4e31db687648d1428cbcaa45759ab79fa5d
Millipede406 <97822695+Millipede406@users.noreply.github.com>
2023-06-04 17:14:21 +0800

    ToolsMenu contents

--- GUI/ToolsMenu.cs
+++ GUI/ToolsMenu.cs
@@ -46,9 +46,44 @@ namespace DebugMod.GUI
         public static void Menu(int windowID)
         {
             //Draw window contents
+            ImprovedScreenshotModeButton();
+
+            ImprovedScreenshotModeSettingsButton();
+
+            DisableFogButton();
+
 
             // Make the window draggable
             UnityEngine.GUI.DragWindow();
+        }
+        private static void ImprovedScreenshotModeButton()
+        {
+            string[] text = { "Disable ISM", "Enable ISMe" };
+            string t = ImprovedScreenshotMode ? text[0] : text[1];
+
+            if (UnityEngine.GUI.Button(rects[1], t))
+            {
+                ImprovedScreenshotMode = !ImprovedScreenshotMode;
+            }
+        }
+        private static void ImprovedScreenshotModeSettingsButton()
+        {
+            string text = "Settings";
+
+            if(UnityEngine.GUI.Button(rects[2], text))
+            {
+                // Enable the ISM Settings Menu
+            }
+        }
+        private static void DisableFogButton()
+        {
+            string[] text = { "Enable Fog", "Disable Fog" };
+            string t = DisableFog ? text[0] : text[1];
+
+            if(UnityEngine.GUI.Button(rects[3], t))
+            {
+                DisableFog = !DisableFog;
+            }
         }
     }
 }
