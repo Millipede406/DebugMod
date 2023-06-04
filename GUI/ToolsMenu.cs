e25236009a80d03cdb4e0f9d5a0f1b9a76330fa5
Millipede406 <97822695+Millipede406@users.noreply.github.com>
2023-06-04 17:04:53 +0800

    Draw ToolsMenu

--- GUI/ToolsMenu.cs
+++ GUI/ToolsMenu.cs
@@ -38,7 +38,15 @@ namespace DebugMod.GUI
 
         public static void DrawMenu()
         {
+            // Draw the window
+            windowRect = UnityEngine.GUI.Window(2, windowRect, (UnityEngine.GUI.WindowFunction)Menu, "Tools Menu");
+        }
+        public static void Menu(int windowID)
+        {
+            //Draw window contents
 
+            // Make the window draggable
+            UnityEngine.GUI.DragWindow();
         }
     }
 }
