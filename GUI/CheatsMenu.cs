9d14dd72c0102638c35f9e0fe47f00ecbac90a89
Millipede406 <97822695+Millipede406@users.noreply.github.com>
2023-06-04 11:00:44 +0800

    Defined CheatsMenu rects

--- GUI/CheatsMenu.cs
+++ GUI/CheatsMenu.cs
@@ -1,8 +1,42 @@
-﻿namespace DebugMod.GUI
+﻿using UnityEngine;
+
+namespace DebugMod.GUI
 {
     public static class CheatsMenu
     {
+        // Cheats Menu
+        // Enable and disable different cheats
+        // - Fast Travel
+        // - Infinite Damage
+        // - Infinite Stamina
+        // - Invulnerability
+        // - No cooldowns
+        // - Shiny Mode
+
         public static bool IsActive;
+        private static Rect[] rects =
+        {
+            // Window rect
+            new Rect(0, 0, 0, 0),
+
+            // Fast Travel rect
+            new Rect(0, 0, 0, 0),
+
+            // Infinite Damage rect
+            new Rect(0, 0, 0, 0),
+
+            // Infinite Stamina rect
+            new Rect(0, 0, 0, 0),
+
+            // Invulnerability rect
+            new Rect(0, 0, 0, 0),
+
+            // No Cooldowns rect
+            new Rect(0, 0, 0, 0),
+
+            // Shiny Mode rect
+            new Rect(0, 0, 0, 0)
+        };
 
         public static void DrawMenu()
         {
