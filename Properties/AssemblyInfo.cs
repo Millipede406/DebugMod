30d7fa3073f6f9eafb4151422717eda51a2a5f1f
Millipede406 <97822695+Millipede406@users.noreply.github.com>
2023-06-05 09:55:30 +0800

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -34,10 +34,10 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
 
-[assembly: MelonInfo(typeof(ModMain), "Debug Mod", "1.1.0", "Millimedia Games")]
+[assembly: MelonInfo(typeof(ModMain), "Debug Mod", "1.2.0-pre1", "Millimedia Games")]
 [assembly: MelonColor(ConsoleColor.Green)]
 [assembly: MelonAuthorColor(ConsoleColor.Magenta)]
 [assembly: MelonAdditionalDependencies("ModdingUtilities")]
