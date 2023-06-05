f69157c93c899be31f61ce83ef30dcb31d513389
Millipede406 <97822695+Millipede406@users.noreply.github.com>
2023-06-05 10:18:20 +0800

    Added FeatureManager

---
+++ Features/FeatureManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugMod.Features
+{
+    public static class FeatureManager
+    {
+        public static void Initialize()
+        {
+
+        }
+
+        public static void Update()
+        {
+
+        }
+    }
+}
