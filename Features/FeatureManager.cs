1163fbfd7af23df5cb81d6859eb292448ebbf8e3
Millipede406 <97822695+Millipede406@users.noreply.github.com>
2023-06-05 10:40:13 +0800

    Initialize FeatureManager

--- Features/FeatureManager.cs
+++ Features/FeatureManager.cs
@@ -10,15 +10,56 @@ namespace DebugMod.Features
 {
     public static class FeatureManager
     {
+        public static List<IFeature> features;
+
         public static void Initialize()
         {
-
+            features = GetAllFeatures();
         }
 
         public static void Update()
         {
             // Updating Invulnerability
             Invulnerability.Update();
+        }
+
+        private static List<IFeature> GetAllFeatures()
+        {
+            // Initializing list
+            List<IFeature> f = new List<IFeature>();
+
+            // Getting features from each category
+            f.AddRange(GetAllCheats());
+
+            f.AddRange(GetAllVisual());
+
+            // Returning result
+            return f;
+        }
+
+        private static List<IFeature> GetAllCheats()
+        {
+            // Initializing list
+            List<IFeature> cheats = new List<IFeature>();
+
+            // Creating instance of features
+            cheats.Add(new Invulnerability());
+            cheats.Add(new InfiniteStamina());
+
+            // Returning list of features
+            return cheats;
+        }
+
+        private static List<IFeature> GetAllVisual()
+        {
+            // Initializing list
+            List<IFeature> visual = new List<IFeature>();
+
+            // Creating instance of features
+            // (there is nothing here yet)
+
+            // Returning the list of features
+            return visual;
         }
     }
 }
