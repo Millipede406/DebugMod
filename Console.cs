fc772167c00f7a0ce57f844231583aac82bf99c2
Millipede406 <97822695+Millipede406@users.noreply.github.com>
2023-06-06 16:29:37 +0800

    LogType enum

--- Console.cs
+++ Console.cs
@@ -2,6 +2,17 @@
 
 namespace DebugMod
 {
+    public enum LogType
+    {
+        // Initialization
+        Init_Main,
+        Init_GUI,
+        Init_Features,
+
+        // Probably other things will go here
+
+    }
+
     public static class Console
     {
 
