c6a95c36dba512a19c5c7fe488099c90fe65640f
Millipede406 <97822695+Millipede406@users.noreply.github.com>
2023-06-06 16:34:19 +0800

    PrefixManager

--- Console.cs
+++ Console.cs
@@ -14,5 +14,28 @@ namespace DebugMod
             // Probably other things will go here
 
         }
+
+        /// <summary>
+        /// Sends a formatted message to the console.
+        /// Format is consistent, so it looks nice and makes me happy
+        /// </summary>
+        /// <param name="l">Log type</param>
+        /// <param name="m">Message</param>
+        public static void Log(LogType l, string m)
+        {
+            string prefix = PrefixManager.GetPrefix(l);
+
+            string message = prefix + " " + m;
+
+            MelonLoader.MelonLogger.Msg(message);
+        }
+
+        public class PrefixManager
+        {
+            public static string GetPrefix(LogType l)
+            {
+
+            }
+        }
     }
 }
